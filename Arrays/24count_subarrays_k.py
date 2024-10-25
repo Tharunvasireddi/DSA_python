6405c2e7b210841baafa4aa19d9c674e2602799c
Tharunvasireddi <tharunvasireddy73@gmail.com>
2024-10-25 07:11:37 +0530

    leaders of array is added

--- Arrays/24count_subarrays_k.py
+++ Arrays/24count_subarrays_k.py
@@ -14,3 +14,20 @@ print("count of subaaray with sum k is :",max_len)
 
 
 #optimal solution
+def Count_subarray(num1,k):
+     sum=0
+     count=0
+     map={}
+     for i in range(len(num1)):
+         sum+=num1[i]
+         if sum==k:
+             count+=1
+         diff=sum-k
+         if diff in map:
+             count+=1
+         else:
+             map[sum]=i
+     return count
+mylist=[2,1,3,4,1,2,1,5,4] 
+max_len=Count_subarray(mylist,6)
+print("count of subarray with sum k is :",max_len)
---
+++ Arrays/25Leaders_in_array.py
@@ -0,0 +1,38 @@
+#brute force
+def leaders(num1):
+    lead=float('-inf')
+    leaders=[]
+    for i in range(len(num1)):
+        lead=0
+        for j in range(i+1,len(num1)):
+            if num1[i]>num1[j]:
+                lead=num1[i]
+            else:
+                lead=0
+                break
+        if lead>0:
+            leaders.append(lead)
+    if num1[-1]>0:
+        leaders.append(num1[-1])
+    return leaders
+mylist=[16,17,4,5,1,2]    
+print("leaders of the array is : ",leaders(mylist))
+
+#optimal soltion
+def Leaders(num1):
+     last=len(num1)-1
+     left=last-1
+     lead=0
+     leaders=[]
+     while left>=0:
+         if num1[last]<num1[left]:
+             leaders.append(num1[left])
+             last=left
+             last=left
+         else:
+             left-=1
+     return leaders
+ 
+mylist=[16,17,4,5,1,2]    
+print("leaders of the array is : ",leaders(mylist))   
+        
