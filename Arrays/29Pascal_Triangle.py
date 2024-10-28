cc7bcd71b8ab6083327d373ed589c3553184070b
Tharunvasireddi <tharunvasireddy73@gmail.com>
2024-10-28 18:35:24 +0530

    pascal triangle is added

---
+++ Arrays/29Pascal_Triangle.py
@@ -0,0 +1,10 @@
+#printing nth value of pascal triangl
+def pascal(row,col):
+    res=1
+    for i in range (col):
+        res=res*(row-i)
+        res=res//(i+1)       
+    return res
+row=int(input("enter the row number : "))
+col=int(input("enter the column number :  "))
+print(f"the nth value of the pascal triangle is {pascal(row,col)}")
