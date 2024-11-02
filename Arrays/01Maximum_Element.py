a20e104845dccccd5d118502f2e5e64c5f7d441d
pravallika1503 <23341A05I6@gmrit.edu.in>
2024-11-02 21:58:26 +0530

    Update 01Maximum_Element.py

--- Arrays/01Maximum_Element.py
+++ Arrays/01Maximum_Element.py
@@ -1,10 +1,10 @@
 #solution
-def Max(my_list):
+def Max(my_list):     # max function
     max=float("-inf") # instialising with minimum value
-    for i in my_list:
-        if max<i:     #checking the elements in the array 
-            max=i
-    return max # retrun maximum elemenet 
+    for i in my_list: #traverse the list using for loop
+        if max<i:     #check condition
+            max=i     #if condition is true update max
+    return max        
 my_list=[1,10,100,50,30,3,4]
 max=Max(my_list)
 print("the maximum element of the given array is :",max)
