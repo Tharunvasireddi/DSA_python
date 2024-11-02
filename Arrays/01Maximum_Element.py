fcbfbccf5896f8e3f2f7175a7f27af38b31150d8
Tharunvasireddi <tharunvasireddy73@gmail.com>
2024-11-02 21:57:04 +0530

    maximum value is array is added

--- Arrays/01Maximum_Element.py
+++ Arrays/01Maximum_Element.py
@@ -2,9 +2,9 @@
 def Max(my_list):
     max=float("-inf") # instialising with minimum value
     for i in my_list:
-        if max<i:
+        if max<i:     #checking the elements in the array 
             max=i
-    return max
+    return max # retrun maximum elemenet 
 my_list=[1,10,100,50,30,3,4]
 max=Max(my_list)
 print("the maximum element of the given array is :",max)
