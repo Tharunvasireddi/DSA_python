afa0c884811a4801b7eb06c2482887ed2886df3a
Tharunvasireddi <tharunvasireddy73@gmail.com>
2024-11-05 19:36:39 +0530

    upperbound file is added

---
+++ Arrays/BinarySearch/03UpperBound.py
@@ -0,0 +1,27 @@
+# bruet force algorithm
+def lower_bound(nums,tar):
+    ind=None
+    for i in range(len(nums)):
+        if nums[i]>tar:
+            ind=i
+            return ind
+    return -1
+arr=[1,2,3,4,5,6,11,18,19]
+tar=int(input("enter the target to search : "))
+print(f"the lower bound of the element is : {lower_bound(arr,tar)}")
+
+#optimal solution 
+def lowerbound(nums,tar):
+    high=len(nums)-1
+    low=0
+    while low<=high:
+        mid=(low+high)//2
+        if nums[mid]>tar:
+            ind=mid
+            high=mid-1
+        else:
+            low=mid+1
+    return ind
+arr=[1,2,3,4,5,6,11,18,19]
+tar=int(input("enter the target to search : "))
+print(f"the lower bound of the element is : {lower_bound(arr,tar)}")
