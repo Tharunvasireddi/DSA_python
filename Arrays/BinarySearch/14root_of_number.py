bf969ffcc892000345c5dfcfdb434c3acab1af7a
Tharunvasireddi <tharunvasireddy73@gmail.com>
2024-11-12 19:17:24 +0530

    root of the number is added

--- Arrays/BinarySearch/14root_of_number.py
+++ Arrays/BinarySearch/14root_of_number.py
@@ -1,8 +1,8 @@
 #brute force algorithm
 def root_of_number(num):
     root=-1
-    for i in range(num//2+1):
-        if i*i <=num:
+    for i in range(num//2+1): #iterating the for loop 
+        if i*i <=num: #checking the i is it sqaure root or not 
             root=i
     return root
 num=int(input("enter the number : "))
@@ -13,16 +13,18 @@ else:
     
 #optimal solution 
 def  square_root(n):
+    if n==1: #return in 1 if number == 1
+        return 1
     low=1
-    high=n//2
+    high=n//2 #intilaisng high with n//2 to decrease the iterations
     root=0
     while low<=high:
         mid=(low+high)//2
-        if mid*mid<=n:
+        if mid*mid<=n: # checking the mid is it squre root of the number or not and also less then  or not 
             root=mid
-            low=mid+1
+            low=mid+1 #if it is true go futher for the correct value
         else:
-            high=mid-1
+            high=mid-1 # if it is false then going for lesser values 
     return root
 num=int(input("enter the number : "))
 if num>0:
