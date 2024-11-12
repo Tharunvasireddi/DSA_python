e5eb8d500ec46d9466d77e5c926d034868970400
Tharunvasireddi <tharunvasireddy73@gmail.com>
2024-11-12 15:49:31 +0530

    root of a number is added

---
+++ Arrays/BinarySearch/14root_of_number.py
@@ -0,0 +1,18 @@
+#brute force algorithm
+def  square_root(n):
+    low=1
+    high=n//2
+    root=0
+    while low<=high:
+        mid=(low+high)//2
+        if mid*mid<=n:
+            root=mid
+            low=mid+1
+        else:
+            high=mid-1
+    return root
+num=int(input("enter the number : "))
+if num>0:
+  print(f"the Square root of the number is : {square_root(num)}")
+else:
+    print("value error")
